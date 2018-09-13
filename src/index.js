93ac00a95bbb200fa0f2291c32e122d9bd820b95
Kvinto1986 <kvinto1986@gmail.com>
2018-09-13 21:25:40 +0300

    task4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var str=equation;
+    str=str.replace(/\s+/g,'');
+    var formula;
+    formula=str.match(/\-?\d{2,}/g);
+    var a=new Number(formula[0]);
+    var b=new Number(formula[1]);
+    var c=new Number(formula[2]);
+    var x1, x2, d;
+    d = b*b - 4*a*c;
+    x1 = (-b - Math.sqrt(d))/(2*a);
+    x2 = (-b + Math.sqrt(d))/(2*a);
+    x1=Math.round(x1);
+    x2=Math.round(x2);
+    if(x2<x1)
+    var result=[x2,x1];
+    else result=[x1,x2];
+    return result;
+
+
 }
